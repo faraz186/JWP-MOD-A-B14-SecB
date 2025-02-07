06bfeb27a164722a044887f2d4206dcfea79f1a3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-07 21:19:53 +0500

    Add files via upload

---
+++ JS/class 01/app.js
@@ -0,0 +1,25 @@
+// there are 3 ways to check output of your program
+
+// on browser
+
+// alert()             ====> JS builtin function
+
+alert(545645646);
+
+// document.write()    ====> JS builtin method
+
+document.write(5000000000000);
+
+// console.log()       ====> JS builtin method
+
+aler(54564564);
+
+alert("Welcome user");
+
+console.log("Welcome user");
+
+document.write("Welcome user");
+
+// 1) builtin
+
+// 2) user defined
