bcf3571b5e3ea144e209e446a432264c9549a821
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-11 21:29:46 +0500

    Chapter 2 - 8 complete...

---
+++ JS/class 02/app.js
@@ -0,0 +1,74 @@
+// alert("Welcome To learning JS...");
+
+// document.write("<h1><i>Learning JS...</i></h1>");
+
+// alert("Welcome \n To \n learning JS...");
+
+// var a = 5;
+// var b = 8;
+// var c = a+b;
+// alert(c)
+
+// var x = "ali";
+// var y = 10;
+// var z = x+y;
+// alert(z)
+
+// var f = "15";
+// var g = 25;
+// var h = f+g;
+// alert(h);
+
+// var r="rohan";
+// var s = 3;
+// var t = r*s;
+// alert(t)
+
+// var a = "7";
+// var b = 9;
+// var c = a/b;
+// alert(c)
+
+// var a = 9/3+(4-6)*2
+// alert(a)
+
+// var a;
+// a=5;
+
+// var x = 10;
+// var y = x%3;
+// alert(y);
+
+// i++ (Post Increment) (Pehlay i ko print krayga phir ek add krayga)
+// ++i (Pre Increment) (Pehlay ek add krayga phir i ko print krayga)
+// i-- (Post Decrement)
+// --i (Pre Decrement)
+
+// var a = 5;
+// var b = a++ + ++a;
+5 + 7;
+// alert(b);
+
+// var x = 3;
+// var y = x++ + x - x-- + --x + ++x - x-- + x + --x - x;
+// 3 + 4 - 4 + 2 + 3 - 3 + 2 + 1 - 1
+// alert(y);
+
+// var a = 2;
+// var b = --a + a + a++ - a-- + a + --a + a-- - a++ - --a;
+// // 1 + 1 + 1 - 2 + 1 + 0 + 0 - -1 - -1
+// alert(b);
+
+
+var quizApp = 10;
+
+
+// Q1
+
+
+
+
+
+// Q2
+
+
