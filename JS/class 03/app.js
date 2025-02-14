359d01b2bc1431fddc2dc724f811c17d8a39678f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-14 21:22:20 +0500

    Chapter 2 - 9 complete...

---
+++ JS/class 03/app.js
@@ -0,0 +1,192 @@
+// variables
+
+// Q: find the area of circle where the radius is 15.23
+
+// var radius = 15.23;
+
+// var areaOfCircle = 3.141 * radius * radius;
+
+// console.log("The area of circle is " + areaOfCircle);
+
+//   varaible name = variable value
+
+// area of cirlce = 3.141 * 15.23 * 15.23
+
+// there are two types of variable in js
+
+// 1) Variable decalaration
+
+// var num;
+
+// console.log(num);
+
+// 2) Variable decalaration and intialization
+
+// var num = 10;
+
+// Data Types in JS
+
+// String    "45"
+
+// Number    45
+
+// undefined
+
+// null
+
+// var flag = null;
+
+// console.log(flag);
+
+// +
+
+// var firstName = 45;
+
+// var lastName = "faraz";
+
+// console.log(firstName * lastName);
+
+// console.log(typeof (firstName + lastName));
+
+// var firstName = "Muhammad";
+
+// var lastName = "faraz";
+
+// alert(firstName + lastName);
+
+// console.log(firstName + " " + lastName);
+
+// console.log(firstName + lastName);
+
+// var num;
+
+// num = 50;
+
+// console.log(num);
+
+// legal variable
+
+// var user = 'faraz';
+
+// Camel Case
+
+// var firstName;
+
+// var userName;
+
+// var whatsApp;
+
+// var fullName;
+
+// Snake Case
+
+// var first_name;
+
+// var user_name;
+
+// var whats_App;
+
+// Pascal Case
+
+// var FirstName;
+
+// var WhatsApp;
+
+// var firstname = 'faraz';
+
+// var lastname = 'faraz';
+
+// var fullname = 'faraz';
+
+// var username = 'faraz';
+
+// var num1 = 500;
+
+// console.log(num1);
+
+// i++  (Post Icrement)
+
+// ++i  (Pre Increment)
+
+// i--  (Post Decrement)
+
+// --i  (Pre-Decrement)
+
+// var num = 5;
+
+// num = num + 1;
+
+// var num2 = --num;
+
+// console.log(num2);
+
+// var a = 4;
+
+// var b = 2;
+
+// var exp = a - --b + ++a - --a - --b + a;
+
+// 4 - 1 + 5 - 4 - 0 + 4;
+
+// console.log(4 - 1 + 5 - 4 - 0 + 4);
+
+// JS (ES5)
+// BASIC JS
+// VANILLA JS
+// COMMON JS
+
+// JS (ES6) (Advance JS)
+
+// var firstName = "Muhammad";
+
+// var lastName = "faraz";
+
+// var age = 24;
+
+// console.log("Hello my name is Muhammad faraz and I'm 24 years old.");
+
+// alert("Hello my name is ", firstName, lastName, " and I'm ", age, "years old.");
+
+// var li1 = "list1";
+// var li2 = "list2";
+// var li3 = "list3";
+
+// document.write("<ul><li>" + li1 + "</li><li>", li2, "</li></ul>");
+
+// alert("Learning \n JS");
+
+// console.log("Learning \n JS");
+
+// document.write("<h1>Learning <br> JS</h1>");
+
+// Prompt
+
+// prompt("Enter email"); // JS builtin function
+
+// var firstName = prompt("Enter first name");
+
+// var lastName = prompt("Enter last name");
+
+// console.log("Hello my name is ", firstName, lastName);
+
+// var firstName = +prompt("Enter first nnumber");
+
+// var lastName = +prompt("Enter second number");
+
+// console.log(firstName + lastName);
+
+// chapter 01 (Alert)
+
+// q1
+
+alert("Welcome user");
+
+// q2
+
+alert("Error please enter a valid password");
+
+// q4
+
+alert("Welcome to JS Land");
+
+alert();
