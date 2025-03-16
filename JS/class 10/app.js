ab7f07a62598a20bf04c6a699b8e7e3c00772901
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-03-16 13:32:21 +0500

    Add files via upload

---
+++ JS/class 10/app.js
@@ -0,0 +1,107 @@
+// function
+
+// function greet() {
+//   console.log("hello function...");
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//   greet();
+// }
+
+// var num1 = +prompt("Enter any number");
+
+// var num2 = +prompt("Enter any number");
+
+function add(a = 20, b) {
+  // parameter
+
+  console.log(a + b);
+}
+
+// add(50); // argument
+
+// function multi() {
+//   return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// }
+
+// var data_received = multi();
+
+// console.log(data_received);
+
+// var arr = [1, 2, 3, 4, 5, 6];
+
+// var copy = arr.slice(0, 4);
+
+// console.log(copy);
+
+// calculator program
+
+// function calculator(num1, num2, opr) {
+//   if (opr === "+") {
+//     return num1 + num2;
+//   } else if (opr === "-") {
+//     return num1 - num2;
+//   } else if (opr === "*") {
+//     return num1 * num2;
+//   } else if (opr === "/") {
+//     return num1 / num2;
+//   } else if (opr === "%") {
+//     return num1 % num2;
+//   } else {
+//     return "Invalid operator";
+//   }
+// }
+
+// var result = calculator(
+//   +prompt("Enter first number"),
+//   +prompt("Enter second number"),
+//   prompt("Enter operator")
+// );
+
+// console.log(result);
+
+// function abc(name) {
+//   return name;
+// }
+// console.log(abc("faraz"));
+
+// function user() {
+//   var num = 50;
+// }
+// console.log(num);
+
+// user();
+
+// var dayOfWeek = prompt("Enter what day of week?");
+
+// switch (dayOfWeek) {
+//   case "Monday":
+//     alert("working day");
+//     break;
+//   case "Tuesday":
+//     alert("working day");
+//     break;
+
+//   case "Wednesday":
+//     alert("working day");
+//     break;
+
+//   case "Thursday":
+//     alert("working day");
+//     break;
+
+//   case "Friday":
+//     alert("working day");
+//     break;
+
+//   case "Saturday":
+//     alert("fun day");
+//     break;
+
+//   case "Sunday":
+//     alert("fun day");
+//     break;
+
+//   default:
+//     alert("Incorrect input..");
+// }
