ab5483cc10f9343ad3ce6763cea3a04d09fc5d26
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-05-02 19:04:03 +0500

    Update app.js

--- JS/TodoApp with firebase db/app.js
+++ JS/TodoApp with firebase db/app.js
@@ -2,7 +2,7 @@ var firebaseConfig = {
   apiKey: "AIzaSyAWkR-DoNy-zydZkR8qwC0e0DPcPKt2ua0",   
   authDomain: "todoapp-5c2a4.firebaseapp.com", 
   databaseURL: "https://todoapp-5c2a4-default-rtdb.firebaseio.com",
-  projectId: "todoapp-5c2a4",
+  projectId: "todoapp-5c2a4", 
   storageBucket: "todoapp-5c2a4.firebasestorage.app",
   messagingSenderId: "99782828360", 
   appId: "1:99782828360:web:d80874b2f76aab13c3d37d",
